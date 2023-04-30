8f991791315af4b71d1a3d4e8b97be150580c731
VTrace <valdhiesetiawan@gmail.com>
2023-04-30 20:53:21 +0800

    Add produk menu link, discover how to make the link active and deactive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -130,24 +130,27 @@
         <ul class="sidebar-nav" id="sidebar-nav">
 
             <li class="nav-item">
-                <a class="nav-link " href="{{ route('dashboard') }}">
+                <a class="nav-link {{ Request::routeIs('dashboard') ? '' : 'collapsed' }}" href="{{ route('dashboard') }}">
                     <i class="bi bi-grid"></i>
                     <span>Dashboard</span>
                 </a>
-            </li><!-- End Dashboard Nav -->
+            </li>
+
+            <li class="nav-heading">Produk</li>
+
             <li class="nav-item">
-                <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-                    <i class="bi bi-menu-button-wide"></i><span>Components</span><i
-                        class="bi bi-chevron-down ms-auto"></i>
+                <a class="nav-link collapsed" href="#">
+                <i class="bi bi-person"></i>
+                <span>Kategori</span>
                 </a>
-                <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-                    <li>
-                        <a href="components-alerts.html">
-                            <i class="bi bi-circle"></i><span>Alerts</span>
-                        </a>
-                    </li>s
-                </ul>
-            </li><!-- End Components Nav -->
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#">
+                <i class="bi bi-person"></i>
+                <span>Produk</span>
+                </a>
+            </li>
         </ul>
 
     </aside><!-- End Sidebar-->
