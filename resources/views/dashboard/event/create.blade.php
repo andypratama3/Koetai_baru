dde21b2c6c0132f3d543f7d8acfcf4ba66b0c86a
VTrace <valdhiesetiawan@gmail.com>
2023-04-30 23:13:10 +0800

    Add flash message in create form

--- resources/views/dashboard/event/create.blade.php
+++ resources/views/dashboard/event/create.blade.php
@@ -13,44 +13,53 @@
     <div class="col-lg-12">
         <div class="card">
             <div class="card-body">
-              <h5 class="card-title">General Form Elements</h5>
-
-              <!-- General Form Elements -->
-              <form action="{{ route('dashboard.event.store') }} " method="POST" enctype="multipart/form-data">
-                @csrf
-                <div class="row mb-3">
-                  <label for="inputText" class="col-sm-2 col-form-label">Nama Event</label>
-                  <div class="col-sm-10">
-                    <input type="text" class="form-control" name="nama">
-                  </div>
+                <h5 class="card-title">Tambah Event</h5>
+                @if (count($errors) > 0)
+                <div class="alert alert-danger bg-danger text-light border-0 alert-dismissible fade show" role="alert">
+                    <h5><i class="bi bi-ban"></i>Peringatan!</h5>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert"
+                        aria-label="Close"></button>
                 </div>
-                <div class="row mb-3">
-                    <label for="inputDate" class="col-sm-2 col-form-label">Tanggal</label>
-                    <div class="col-sm-10">
-                        <input type="text" name="tanggal"  class="form-control"/>
+                @endif
+                <!-- General Form Elements -->
+                <form action="{{ route('dashboard.event.store') }} " method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="row mb-3">
+                        <label for="inputText" class="col-sm-2 col-form-label">Nama Event</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="nama">
+                        </div>
                     </div>
-                </div>
-                <div class="row mb-3">
-                  <label for="inputNumber" class="col-sm-2 col-form-label">File Foto</label>
-                  <div class="col-sm-10">
-                    <input class="form-control" type="file" id="formFile" name="foto">
-                  </div>
-                </div>
+                    <div class="row mb-3">
+                        <label for="inputDate" class="col-sm-2 col-form-label">Tanggal</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="tanggal" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label for="inputNumber" class="col-sm-2 col-form-label">File Foto</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="file" id="formFile" name="foto">
+                        </div>
+                    </div>
 
-                <div class="row mb-3">
-                    <label for="inputEmail" class="col-sm-2 col-form-label">Deskripsi</label>
-                <div class="quill-editor-full form-control"></div>
+                    <div class="row mb-3">
+                        <label for="inputEmail" class="col-sm-2 col-form-label">Deskripsi</label>
+                        <div class="quill-editor-full form-control"></div>
 
-                <div class="row mt-3 mb-3 text-center">
-                  <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary">Simpan</button>
-                  </div>
-                </div>
+                        <div class="row mt-3 mb-3 text-center">
+                            <div class="col-sm-10">
+                                <button type="submit" class="btn btn-primary">Simpan</button>
+                            </div>
+                        </div>
 
-              </form><!-- End General Form Elements -->
+                </form><!-- End General Form Elements -->
 
             </div>
-          </div>
+        </div>
     </div>
 </div>
 @endsection
@@ -62,15 +71,15 @@
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 
 <script>
-    $(function() {
-      $('input[name="tanggal"]').daterangepicker({
+$(function () {
+    $('input[name="tanggal"]').daterangepicker({
         timePicker: true,
         startDate: moment().startOf('hour'),
         endDate: moment().startOf('hour').add(32, 'hour'),
         locale: {
-          format: 'M/DD hh:mm A'
+            format: 'M/DD hh:mm A'
         }
-      });
     });
-    </script>
+});
+</script>
 @endpush
