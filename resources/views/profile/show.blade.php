1aed94454b9c078e5d29521d3488fbd39c4349d1
andypratama3 <andypratama1211@gmail.com>
2023-06-16 20:33:02 +0800

    Update Password

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,25 +1,141 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
+@section('title','Profil')
+@section('content')
 
-    <div>
-        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-            @if (Laravel\Fortify\Features::canUpdateProfileInformation())
-                @livewire('profile.update-profile-information-form')
+<div class="row">
+    <!-- Left side columns -->
+    <div class="col-lg-12">
+          <section class="section profile">
+            <div class="row">
+              <div class="col-xl-4">
 
-                <x-section-border />
-            @endif
+                <div class="card">
+                  <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
 
-            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
-                <div class="mt-10 sm:mt-0">
-                    @livewire('profile.update-password-form')
+                    <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+                    <h2>{{ auth()->user()->name }}</h2>
+                    <h3>{{ auth()->user()->role == '1' ? 'Admin' : 'User' }}</h3>
+                  </div>
                 </div>
+              </div>
+              <div class="col-xl-8">
 
-                <x-section-border />
-            @endif
+                <div class="card">
+                  <div class="card-body pt-3">
+                    <!-- Bordered Tabs -->
+                    <ul class="nav nav-tabs nav-tabs-bordered">
+
+                      <li class="nav-item">
+                        <button class="nav-link active" data-bs-toggle="tab" data-bs-target="#profile-overview">Overview</button>
+                      </li>
+
+                      <li class="nav-item">
+                        <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-edit">Edit Profile</button>
+                      </li>
+                      <li class="nav-item">
+                        <button class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-change-password">Change Password</button>
+                      </li>
+
+                    </ul>
+                    <div class="tab-content pt-2">
+
+                      <div class="tab-pane fade show active profile-overview" id="profile-overview">
+
+                        <h5 class="card-title">Profile Details</h5>
+
+                        <div class="row">
+                          <div class="col-lg-3 col-md-4 label ">Full Name</div>
+                          <div class="col-lg-9 col-md-8">{{ auth()->user()->name }}</div>
+                        </div>
+
+                        <div class="row">
+                          <div class="col-lg-3 col-md-4 label">Role</div>
+                          <div class="col-lg-9 col-md-8">{{ auth()->user()->role == '1' ? 'Admin' : 'User' }}</div>
+                        </div>
+
+
+                        <div class="row">
+                          <div class="col-lg-3 col-md-4 label">Email</div>
+                          <div class="col-lg-9 col-md-8">{{ auth()->user()->email }}</div>
+                        </div>
+
+                      </div>
+
+                      <div class="tab-pane fade profile-edit pt-3" id="profile-edit">
+
+                        <!-- Profile Edit Form -->
+                        <form action="{{ route('user-profile-information.update') }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                          <div class="row mb-3">
+                            <label for="profileImage" class="col-md-4 col-lg-3 col-form-label">Profile Image</label>
+                            <div class="col-md-8 col-lg-9">
+                              <img src="assets/img/profile-img.jpg" alt="Profile">
+                              <div class="pt-2">
+                                <a href="#" class="btn btn-primary btn-sm" title="Upload new profile image"><i class="bi bi-upload"></i></a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Remove my profile image"><i class="bi bi-trash"></i></a>
+                              </div>
+                            </div>
+                          </div>
+
+                          <div class="row mb-3">
+                            <label for="fullName" class="col-md-4 col-lg-3 col-form-label">Full Name</label>
+                            <div class="col-md-8 col-lg-9">
+                              <input name="name" type="text" class="form-control" id="fullName" value="{{ auth()->user()->name }}">
+                            </div>
+                          </div>
+                          <div class="row mb-3">
+                            <label for="fullName" class="col-md-4 col-lg-3 col-form-label">Email</label>
+                            <div class="col-md-8 col-lg-9">
+                              <input name="email" type="text" class="form-control" id="email" value="{{ auth()->user()->email }}">
+                            </div>
+                          </div>
+
+                          <div class="text-center">
+                            <button type="submit" class="btn btn-primary">Save Changes</button>
+                          </div>
+                        </form><!-- End Profile Edit Form -->
+
+                      </div>
+
+                      <div class="tab-pane fade pt-3" id="profile-change-password">
+                        <!-- Change Password Form -->
+
+                        <form action="{{ route('user-password.update') }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                          <div class="row mb-3">
+                            <label for="currentPassword" class="col-md-4 col-lg-3 col-form-label">Current Password</label>
+                            <div class="col-md-8 col-lg-9">
+                              <input type="password" class="form-control" name="current_password" id="currentPassword" wire:model.defer="state.current_password">
+                            </div>
+                          </div>
+
+                          <div class="row mb-3">
+                            <label for="newPassword" class="col-md-4 col-lg-3 col-form-label">New Password</label>
+                            <div class="col-md-8 col-lg-9">
+                              <input name="password" type="password" class="form-control" id="newPassword">
+                            </div>
+                          </div>
+
+                          <div class="row mb-3">
+                            <label for="renewPassword" class="col-md-4 col-lg-3 col-form-label">Re-enter New Password</label>
+                            <div class="col-md-8 col-lg-9">
+                              <input name="password_confirmation" type="password" class="form-control" wire:model.defer="state.password_confirmation" name="password_confirmation" id="renewPassword">
+                            </div>
+                          </div>
+
+                          <div class="text-center">
+                            <button type="submit" class="btn btn-primary">Change Password</button>
+                          </div>
+                        </form><!-- End Change Password Form -->
+                      </div>
+                    </div><!-- End Bordered Tabs -->
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
 
             @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
                 <div class="mt-10 sm:mt-0">
@@ -40,6 +156,9 @@
                     @livewire('profile.delete-user-form')
                 </div>
             @endif
+            </div>
         </div>
     </div>
-</x-app-layout>
+</div>
+
+@endsection
